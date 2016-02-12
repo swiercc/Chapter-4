d46f9a4db397eee3b28d7be668101a1c7b018b0c
swiercc <swiercc62@gmail.com>
2016-02-12 11:32:18 -0500

    bitch nigga tanzos

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input;
+            int sum = 0;
+            for (int i = 1; i > 0; i++)
+            {
+                Console.WriteLine("Enter a number");
+                input = Convert.ToInt32(Console.ReadLine());
+                 while(input == 999)
+                {
+                    break;
+                }
+
+                 sum = sum + input;
+                 Console.WriteLine(sum);
+              
+            }
+        }
+    }
+}
